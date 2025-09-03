71faa6d55a710069b607ff4b94850befd4536d66
Pushwant Singh Sagoo <43817108+Pyrrhusn@users.noreply.github.com>
2025-09-03 13:13:30 +0200

    Update Connectie.java

--- src/persistentie/Connectie.java
+++ src/persistentie/Connectie.java
@@ -2,6 +2,6 @@ package persistentie;
 
 public class Connectie {	
 	
-	public static final String JDBC_URL = "jdbc:mysql://ID399541_G36.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID399541_g36&password=schoolproject2023";
+	public static final String JDBC_URL = "<connection_string>";
 
 }
